34d1c492970790170bf4ff202753ef092e7665f8
matias-simon <matias.simon@hotmail.com>
2023-09-04 18:12:15 -0300

    new push

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,34 +1,41 @@
+import { Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { getInstrumento } from "../../productos/productos";
+import "./ItemDetailContainer.css"; // Update your CSS file
 import ItemDetail from "../ItemDetail/ItemDetail";
 import Loading from "../Loading/Loading";
-import "./ItemDetailContainer";
 import { useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
 import { db } from "../../Services/firebase/firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
-
 
 const ItemDetailContainer = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [item, setItem] = useState({});
-  const { id } = useParams();
+  const { tipo } = useParams(); 
 
   useEffect(() => {
-    const itemRef = doc(db, "items", id)
+    setIsLoading(true);
+    const itemRef = doc(db, "items", tipo);
+
     getDoc(itemRef)
-        .then((resp) => {
-            setItem({ ...resp.data(), id: resp.id }),
-                setIsLoading(false)
-        })           
-  }, [id]);
+      .then((resp) => {
+        setItem({ ...resp.data(), id: resp.id });
+      })
+      .catch((error) => {
+        console.error("Error loading data:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [tipo]);
 
   if (isLoading) return <Loading />;
 
   return (
     <div className="fondo">
-      
-     <ItemDetail item={item}/>
-     
+      <Typography variant="h2" sx={{ textAlign: "center", padding: "1rem" }}>
+        Item Detail
+      </Typography>
+      <ItemDetail item={item} />
     </div>
   );
 };
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -3,32 +3,36 @@ import React, { useEffect, useState } from "react";
 import "./ItemListContainer.css";
 import ItemList from "../ItemList/ItemList";
 import Loading from "../Loading/Loading";
-import { getCategory, getInstrumentos } from "../../productos/productos";
 import { useParams } from "react-router-dom";
 import { getDocs, collection, query, where } from "firebase/firestore";
 import { db } from "../../Services/firebase/firebaseConfig";
 
 const ItemListContainer = ({ Greetings }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [item, setItem] = useState([]);
-  const { tipo } = useParams();
-
+  const [items, setItems] = useState([]);
+  const { tipo } = useParams(); 
   useEffect(() => {
-
+    setIsLoading(true);
     const itemsRef = collection(db, "items");
-    const qry = tipo ? query(itemsRef, where("tipo", "==", tipo)) : itemsRef
+    
+    
+    const qry = tipo ? query(itemsRef, where("tipo", "==", tipo)) : itemsRef;
 
     getDocs(qry)
-        .then((resp) => {
-            setItem(
-                resp.docs.map((doc) => {
-                    return { ...doc.data(), id: doc.id }
-                })
-            )
-        }
-            , setIsLoading(false)
-        )
-}, [tipo])
+      .then((resp) => {
+        setItems(
+          resp.docs.map((doc) => {
+            return { ...doc.data(), id: doc.id };
+          })
+        );
+      })
+      .catch((error) => {
+        console.error("Error loading data:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [tipo]);
 
   if (isLoading) return <Loading />;
 
@@ -37,7 +41,7 @@ const ItemListContainer = ({ Greetings }) => {
       <Typography variant="h2" sx={{ textAlign: "center", padding: "1rem" }}>
         {Greetings}
       </Typography>
-      <ItemList itemList={item} />
+      <ItemList itemList={items} />
     </div>
   );
 };
